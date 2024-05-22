e98d0b91449792b24ae99b907c4f0bdfd2579570
doudouchevalier <edouard.chevalier@student.junia.com>
2024-05-22 14:56:45 +0200

    ajout header sur les pages formulaire et events

--- Projet/formulaire.php
+++ Projet/formulaire.php
@@ -3,14 +3,39 @@
 <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="fr">
 
 <head>
-	<meta charset="UTF-8"/> 
-	<title>BDE Naeptune</title>
+    <meta charset="UTF-8"/> 
+    <title>BDE Naeptune</title>
+    <link href="style/style_footer.css" type="text/css" rel="stylesheet">
+    <link href="style/style_header.css" type="text/css" rel="stylesheet">
+    <link href="style/style_form.css" type="text/css" rel="stylesheet">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css" integrity="sha512-Fo3rlrZj/k7ujTnHg4C+2XonQ6dLLJz1q5Yc6RaO1I5F/4ajEe3K5oS/1jT1UpBs1sTTX9KJXPE6we3vI8STuQ==" crossorigin="anonymous" referrerpolicy="no-referrer" />
 </head>
 
 <body>
 
-    <header>
-        BDE Naeptune
+<header>
+        <nav class="navbar navbar-expand-lg navbar-light">
+            <div class="container-fluid">
+                <a class="navbar-brand" href="accueil.html">
+                    <img src="img/texte_bde_white.png" alt="BDE Naeptune" class="text_logo">
+                    <img src="img/logo_bde.png" alt="Logo Naeptune" class="logo">
+                </a>
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+                <div class="collapse navbar-collapse" id="navbarNav">
+                    <ul class="navbar-nav ms-auto">
+                        <li class="nav-item">
+                            <a class="nav-link" href="events.html">Evenements</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="contact.html">Contact</a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
     </header>
 
     <FORM ACTION='insert_user.php' METHOD='post'>
