aa5850ece9e9ce716059f3cdcc75915f3af97f0a
doudouchevalier <edouard.chevalier@student.junia.com>
2024-05-24 16:30:31 +0200

    commit

--- Projet/accueil.php
+++ Projet/accueil.php
@@ -6,7 +6,6 @@
     <meta charset="UTF-8"/> 
     <title>BDE Naeptune</title>
     <link href="style/style_footer.css" type="text/css" rel="stylesheet">
-    <link href="style/style_header.css" type="text/css" rel="stylesheet">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css" integrity="sha512-Fo3rlrZj/k7ujTnHg4C+2XonQ6dLLJz1q5Yc6RaO1I5F/4ajEe3K5oS/1jT1UpBs1sTTX9KJXPE6we3vI8STuQ==" crossorigin="anonymous" referrerpolicy="no-referrer" />
 </head>
