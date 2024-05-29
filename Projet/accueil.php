db04bc7354850162cd7450a2afa52d9efb1e5f4b
ElvinAlb <elvinprod@hotmail.com>
2024-05-29 08:59:24 +0200

    ajout carte evenement

--- Projet/accueil.php
+++ Projet/accueil.php
@@ -7,6 +7,7 @@
     <title>BDE Naeptune</title>
     <link href="style/style_footer.css" type="text/css" rel="stylesheet">
     <link href="style/style_header.css" type="text/css" rel="stylesheet">
+    <link href="style/style_form.css" type="text/css" rel="stylesheet">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css" integrity="sha512-Fo3rlrZj/k7ujTnHg4C+2XonQ6dLLJz1q5Yc6RaO1I5F/4ajEe3K5oS/1jT1UpBs1sTTX9KJXPE6we3vI8STuQ==" crossorigin="anonymous" referrerpolicy="no-referrer" />
 </head>
@@ -20,29 +21,23 @@
 
         
         <container>
-            
-            <?php
-            include 'db_connection.php';
+        <?php
+        include 'db_connection.php';
 
-            $query =
-                "SELECT idEvent, nom, date, description, capacite FROM evenements WHERE date > CURDATE() ORDER BY idEvent";
-            $result = mysqli_query($link, $query);
-            ?>
-            <?php while ($row = mysqli_fetch_assoc($result)) {
-                echo "<tr>";
-                echo "<td><h2>" .
-                    htmlspecialchars($row["nom"]) .
-                    "</h2></td> ";
-                echo "<td> Date : " . htmlspecialchars($row["date"]) . "</td>";
-                echo "<td> Description : " .
-                    htmlspecialchars($row["description"]) .
-                    "</td>";
-                echo "<td> Nombre max de personnes : " .
-                    htmlspecialchars($row["capacite"]) .
-                    "</td>";
-                echo "</tr><br/>";
-            } ?>
-        </container>
+        $query = "SELECT idEvent, nom, date, description, capacite FROM evenements WHERE date > CURDATE() ORDER BY idEvent";
+        $result = mysqli_query($link, $query);
+        ?>
+        <div class="events-container">
+            <?php while ($row = mysqli_fetch_assoc($result)) { ?>
+                <div class="event-card">
+                    <h2><?php echo htmlspecialchars($row["nom"]); ?></h2>
+                    <p><strong>Date:</strong> <?php echo htmlspecialchars($row["date"]); ?></p>
+                    <p><strong>Description:</strong> <?php echo htmlspecialchars($row["description"]); ?></p>
+                    <p><strong>Nombre max de personnes:</strong> <?php echo htmlspecialchars($row["capacite"]); ?></p>
+                </div>
+            <?php } ?>
+        </div>
+    </container>
 
 
     </main>
